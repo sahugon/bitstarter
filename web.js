65adddf7a5615351acc3e8130acdbed44afba671
sahugon <sahugon@gmail.com>
2013-07-22 19:52:52 +0100

    Mi producto

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/images'));
 app.get('/', function(request, response) {
 	var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
 	response.send(buf.toString());
